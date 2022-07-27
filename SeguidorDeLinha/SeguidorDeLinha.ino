7b891fec1265d62a96af18cc37c53678bab96865
PedroLCamara <plcrlschool@gmail.com>
2022-07-27 12:38:34 -0300

    depressao

--- SeguidorDeLinha/SeguidorDeLinha.ino
+++ SeguidorDeLinha/SeguidorDeLinha.ino
@@ -1,13 +1,16 @@
-//Cor de acordo com iluminação
-int DTBranco = 14;
+// Cor de acordo com iluminação
+int DTBranco =  12;
 
-//Motores
+// Motores
 int MotorRodaDireitaNeg = 10;
 int MotorRodaDireitaPos = 11;
 int MotorRodaEsquerdaNeg = 12;
 int MotorRodaEsquerdaPos = 13;
 
-//Sensor cor 1
+int PotenciaMotorDireita = A0;
+int PotenciaMotorEsquerda = A1;
+
+// Sensor cor 1
 int S1pinS0 = 53;
 int S1pinS1 = 51;
 int S1pinS2 = 49;
@@ -26,7 +29,6 @@ int S3pinS2 = 38;
 int S3pinS3 = 40;
 int S3pinOut = 36;
 
-
 unsigned int S1Verm;
 unsigned int S1Verd;
 unsigned int S1Azul;
@@ -38,34 +40,39 @@ unsigned int S2Bran;
 unsigned int S3Verm;
 unsigned int S3Verd;
 unsigned int S3Azul;
-unsigned int S3Bran; 
+unsigned int S3Bran;
 
-void AndarParaFrente() {
+void AndarParaFrente()
+{
   digitalWrite(MotorRodaDireitaNeg, LOW);
   digitalWrite(MotorRodaEsquerdaNeg, LOW);
   digitalWrite(MotorRodaDireitaPos, HIGH);
   digitalWrite(MotorRodaEsquerdaPos, HIGH);
 }
 
-void AndarParaTras() {
+void AndarParaTras()
+{
   digitalWrite(MotorRodaDireitaNeg, HIGH);
   digitalWrite(MotorRodaEsquerdaNeg, HIGH);
   digitalWrite(MotorRodaDireitaPos, LOW);
   digitalWrite(MotorRodaEsquerdaPos, LOW);
 }
 
-void VirarEsquerda() {
+void VirarEsquerda()
+{
   digitalWrite(MotorRodaEsquerdaPos, LOW);
 }
 
-void VirarDireita() {
+void VirarDireita()
+{
   digitalWrite(MotorRodaDireitaNeg, HIGH);
   digitalWrite(MotorRodaEsquerdaNeg, LOW);
   digitalWrite(MotorRodaDireitaPos, LOW);
   digitalWrite(MotorRodaEsquerdaPos, LOW);
 }
 
-void Parar(){
+void Parar()
+{
   digitalWrite(MotorRodaDireitaNeg, LOW);
   digitalWrite(MotorRodaEsquerdaNeg, LOW);
   digitalWrite(MotorRodaDireitaPos, LOW);
@@ -74,7 +81,7 @@ void Parar(){
 
 void detectarCor()
 {
-  //Sensor S1
+  // Sensor S1
   digitalWrite(S1pinS2, LOW);
   digitalWrite(S1pinS3, LOW);
   S1Verm = pulseIn(S1pinOut, digitalRead(S1pinOut) == HIGH ? LOW : HIGH);
@@ -91,7 +98,7 @@ void detectarCor()
   digitalWrite(S1pinS3, HIGH);
   S1Verd = pulseIn(S1pinOut, digitalRead(S1pinOut) == HIGH ? LOW : HIGH);
 
-  //Sensor S2
+  // Sensor S2
   digitalWrite(S2pinS2, LOW);
   digitalWrite(S2pinS3, LOW);
   S2Verm = pulseIn(S2pinOut, digitalRead(S2pinOut) == HIGH ? LOW : HIGH);
@@ -108,7 +115,7 @@ void detectarCor()
   digitalWrite(S2pinS3, HIGH);
   S2Verd = pulseIn(S2pinOut, digitalRead(S2pinOut) == HIGH ? LOW : HIGH);
 
-  //Sensor 3
+  // Sensor 3
   digitalWrite(S3pinS2, LOW);
   digitalWrite(S3pinS3, LOW);
   S3Verm = pulseIn(S3pinOut, digitalRead(S3pinOut) == HIGH ? LOW : HIGH);
@@ -126,32 +133,31 @@ void detectarCor()
   S3Verd = pulseIn(S3pinOut, digitalRead(S3pinOut) == HIGH ? LOW : HIGH);
 }
 
-void setup() {
-  //Motores
+void setup()
+{
+  // Motores
   pinMode(MotorRodaDireitaNeg, OUTPUT);
   pinMode(MotorRodaDireitaPos, OUTPUT);
   pinMode(MotorRodaEsquerdaNeg, OUTPUT);
   pinMode(MotorRodaEsquerdaPos, OUTPUT);
-  //Sensor S1
+  // Sensor S1
   pinMode(S1pinS0, OUTPUT);
   pinMode(S1pinS1, OUTPUT);
   pinMode(S1pinS2, OUTPUT);
   pinMode(S1pinS3, OUTPUT);
   pinMode(S1pinOut, INPUT);
-  //Sensor S2
+  // Sensor S2
   pinMode(S2pinS0, OUTPUT);
   pinMode(S2pinS1, OUTPUT);
   pinMode(S2pinS2, OUTPUT);
   pinMode(S2pinS3, OUTPUT);
   pinMode(S2pinOut, INPUT);
-  //Sensor S3
+  // Sensor S3
   pinMode(S3pinS0, OUTPUT);
   pinMode(S3pinS1, OUTPUT);
   pinMode(S3pinS2, OUTPUT);
   pinMode(S3pinS3, OUTPUT);
   pinMode(S3pinOut, INPUT);
-
-
 
   Serial.begin(9600);
   digitalWrite(S1pinS0, HIGH);
@@ -160,6 +166,8 @@ void setup() {
   digitalWrite(S2pinS1, LOW);
   digitalWrite(S3pinS0, HIGH);
   digitalWrite(S3pinS1, LOW);
+  analogWrite(PotenciaMotorEsquerda, 130);
+  analogWrite(PotenciaMotorDireita, 130);
 }
 
 bool pretoDireita = false;
@@ -203,20 +211,27 @@ void loop() {
      verdeDireita = false;
      verdeEsquerda = false;
   } 
-  if(S2Bran >= DTBranco && S1Bran >= DTBranco && S3Bran < DTBranco){
-    verdeDireita = false;
-    pretoDireita = false;
+  else if(S2Bran >= DTBranco && S1Bran >= DTBranco && S3Bran >= DTBranco){
     if(S1Verm > S1Verd){
       verdeEsquerda = true;
-      AndarParaFrente();
     }
-    else{
-      if(!verdeEsquerda){
-        AndarParaFrente();
-        pretoEsquerda = true;
-      }
-      else{
-        AndarParaFrente();
+    if(S3Verm > S3Verd){
+      verdeDireita = true;
+    }
+    if(verdeEsquerda && verdeDireita){
+      Parar(); //Temporário, tem que fazer voltar
+    } else if(verdeDireita){
+      AndarParaFrente();
+        delay(500);
+        VirarDireita();
+        delay(1000);
+         do{
+          detectarCor();
+          VirarDireita();
+        }while(S2Bran < DTBranco);
+        pretoEsquerda = false;
+    } else if(verdeEsquerda){
+      AndarParaFrente();
         delay(500);
         VirarEsquerda();
         delay(1000);
@@ -225,21 +240,32 @@ void loop() {
           VirarEsquerda();
         }while(S2Bran < DTBranco);
         pretoDireita = false;
-      }
+    }
+  }
+  else if(S2Bran >= DTBranco && S1Bran >= DTBranco && S3Bran < DTBranco){
+    verdeDireita = false;
+    pretoDireita = false;
+    if(S1Verm > S1Verd){
+        AndarParaFrente();
+        delay(500);
+        VirarEsquerda();
+        delay(1000);
+         do{
+          detectarCor();
+          VirarEsquerda();
+        }while(S2Bran < DTBranco);
+        pretoEsquerda = false;
+        pretoDireita = false;
+    }
+    else{
+        AndarParaFrente();
+        pretoEsquerda = true;
     }
   } 
-  if(S2Bran >= DTBranco && S1Bran < DTBranco && S3Bran >= DTBranco){
+  else if(S2Bran >= DTBranco && S1Bran < DTBranco && S3Bran >= DTBranco){
     verdeEsquerda = false;
     pretoEsquerda = false;
     if(S3Verm > S3Verd){
-      AndarParaFrente();
-      verdeDireita = true;
-    }
-    else{
-      if(!verdeDireita){
-        AndarParaFrente();
-        pretoDireita = true;
-      } else{
         AndarParaFrente();
         delay(500);
         VirarDireita();
@@ -249,10 +275,14 @@ void loop() {
           VirarDireita();
         }while(S2Bran < DTBranco);
         pretoEsquerda = false;
-      }
+        pretoDireita = false;
+    }
+    else{
+      pretoDireita = true;
+      AndarParaFrente();
     }
   }
-  if(S2Bran < DTBranco && S1Bran < DTBranco && S3Bran < DTBranco){
+  else if(S2Bran < DTBranco && S1Bran < DTBranco && S3Bran < DTBranco){
     verdeEsquerda = false;
     verdeDireita = false;
     if(pretoDireita){
@@ -260,6 +290,7 @@ void loop() {
         detectarCor();
         VirarDireita();
       }while(S2Bran < DTBranco);
+      pretoEsquerda = false;
       pretoDireita = false;
     }
     else if(pretoEsquerda){
@@ -268,17 +299,51 @@ void loop() {
         VirarEsquerda();
       }while(S2Bran < DTBranco);
       pretoEsquerda = false;
+      pretoDireita = false;
     }
     else {
       AndarParaFrente();
+      delay(3000);
+      detectarCor();
+      if(S2Bran < DTBranco && S1Bran < DTBranco && S3Bran < DTBranco){
+        AndarParaTras();
+        delay(3000);
+        VirarDireita();
+        delay(500);
+        AndarParaFrente();
+        delay(3000);
+        detectarCor();
+        if(S2Bran < DTBranco && S1Bran < DTBranco && S3Bran < DTBranco){
+          VirarEsquerda();
+          delay(500);
+          AndarParaTras();
+          delay(3000);
+          VirarEsquerda();
+          delay(500);
+          AndarParaFrente();
+          delay(3000);
+          detectarCor();
+          if(S2Bran < DTBranco && S1Bran < DTBranco && S3Bran < DTBranco){
+            VirarDireita();
+            delay(500);
+            AndarParaTras();
+            delay(3000);
+            do{
+              AndarParaFrente();
+              detectarCor();
+            }while(S2Bran < DTBranco);
+          }
+        }
+      }
     }
   }
-  if(S2Bran < DTBranco && S1Bran < DTBranco && S3Bran >= DTBranco){
+  else if(S2Bran < DTBranco && S1Bran < DTBranco && S3Bran >= DTBranco){
       do{
         detectarCor();
         VirarDireita();
       }while(S2Bran < DTBranco);
       pretoDireita = false;
+      pretoEsquerda = false;
   }
   if(S2Bran < DTBranco && S1Bran >= DTBranco && S3Bran < DTBranco){
     do{
@@ -286,5 +351,6 @@ void loop() {
         VirarEsquerda();
       }while(S2Bran < DTBranco);
       pretoEsquerda = false;
+      pretoDireita = false;
   }
 }
